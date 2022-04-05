4b45769639ee26ccfab85fc2d51555e383800d37
GustavoFFelipe <gustavo.ffelipedev@gmail.com>
2022-04-05 17:43:00 -0300

    final front -end index.html

--- scripts/btn-script.js
+++
@@ -1,5 +0,0 @@
-let btn = document.querySelector("#btn-up");
-
-btn.addEventListener("click", function () {
-    window.scrollTo(0, 0), 'slow';
-})
--- scripts/script.js
+++ scripts/script.js
@@ -44,10 +44,25 @@ document.addEventListener("DOMContentLoaded", function() { // On DOM Load initia
 
 
 
-// Up button click
+// Up button click need jquery script in head
 
 let btn = $("#btn-up");
 
 btn.click(function (){
   $('html,body').animate({ scrollTop: 0}, 'slow')
 })
+
+// -------contact button --------------------------------
+
+function initialPopUp(popId) {
+  const pop =  document.getElementById(popId);
+  pop.classList.add('show');
+  pop.addEventListener('click', (e) => {
+    if(e.target.id == popId || e.target.id == 'popup-close'){
+      pop.classList.remove('show');
+    }
+  })
+}
+
+const popBtn = document.getElementById('popup-contact');
+popBtn.addEventListener('click', () => initialPopUp('pop-container'));
