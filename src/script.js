bf4a6aabde86e11dcabc9c25b5aac1716bb168ab
GustavoFFelipe <gustavo.ffelipedev@gmail.com>
2022-04-18 19:31:24 -0300

    mobile index

--- src/script.js
+++ src/script.js
@@ -45,29 +45,20 @@ function setup(){
 	
 }
 
-
-// ------------Up button click need jquery script in head--------
-
-let btn = $("#btn-up");
-
-btn.click(function (){
-  $('html,body').animate({ scrollTop: 0}, 'slow')
-})
-
 // -------contact button -------------------------
 
 function initialPopUp(popId) {
-  const pop =  document.getElementById(popId);
-  pop.classList.add('show');
-  pop.addEventListener('click', (e) => {
-    if(e.target.id == popId || e.target.id == 'popup-close'){
-      pop.classList.remove('show');
-    }
-  })
-}
-
-const popBtn = document.getElementById('popup-contact');
-popBtn.addEventListener('click', () => initialPopUp('pop-container'));
+	const pop =  document.getElementById(popId);
+	pop.classList.add('show');
+	pop.addEventListener('click', (e) => {
+	  if(e.target.id == popId || e.target.id == 'popup-close'){
+		pop.classList.remove('show');
+	  }
+	})
+  }
+  
+  const popBtn = document.getElementById('popup-contact');
+  popBtn.addEventListener('click', () => initialPopUp('popup-container'));
 
 
 
